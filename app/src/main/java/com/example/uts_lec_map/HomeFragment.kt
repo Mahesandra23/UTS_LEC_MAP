f0d9079b8c00cf41b180d901d7edf9f22d9fb852
Ravindra <130423027+Mahesandra23@users.noreply.github.com>
2024-10-21 16:21:28 +0700

    add admin page

--- app/src/main/java/com/example/uts_lec_map/HomeFragment.kt
+++ app/src/main/java/com/example/uts_lec_map/HomeFragment.kt
@@ -44,6 +44,7 @@ class HomeFragment : Fragment() {
         // Setup RecyclerViews dan Bottom Navigation
         setupRecyclerViews()  // Pastikan adapter diinisialisasi
         setupViewPager() // Menyiapkan BottomNavigationView
+        setupBottomNavigation()
 
         // Ambil data dari Firebase
         getBooksFromFirebase()
